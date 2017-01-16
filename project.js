356bf9639d919b8719ae76e0bcd223277ac220e1
Emma <emma30-9@hotmail.com>
2017-01-16 23:49:01 +0100

    Eerste poging barchart

--- project.js
+++ project.js
@@ -1,17 +1,68 @@
-var man_totaal_economie = 0
-var jaren = ["2011", "2012", "2013", "2014", "2015"]
-d3.json("datastudies.json", function(data){
-  data1 = data["2011"]["economie"];
-  console.log(data1);
-  for (i in data1){
-    var obj = data1[i];
-    for (var prop in obj) {
-      obj2 = obj[prop];
-      for (var prop2 in obj2){
-        man_totaal_economie = man_totaal_economie + obj2["man"];
+// Emma van Proosdij 10663657
+window.onload = function() {
 
-      }
-      }
-  }
-  console.log(man_totaal_economie/2);
-})
+  var sections = ['economie', 'gedrag en maatschappij', 'gezondheidszorg', 'natuur', 'onderwijs', 'recht', 'taal en cultuur', 'landbouw en natuurlijke omgeving', 'sectoroverstijgend', 'techniek']
+  var n = 10, // number of samples
+      m = 2; // number of series
+
+  var data = d3.range(m).map(function() { return d3.range(n).map(Math.random); });
+
+  var margin = {top: 20, right: 30, bottom: 30, left: 40},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+  var y = d3.scale.linear()
+      .domain([0, 1])
+      .range([height, 0]);
+
+  var x0 = d3.scale.ordinal()
+      .domain(d3.range(n))
+      .rangeBands([0, width], .2);
+
+  var x1 = d3.scale.ordinal()
+      .domain(d3.range(m))
+      .rangeBands([0, x0.rangeBand()]);
+
+  var z = d3.scale.category10();
+
+  var xAxis = d3.svg.axis()
+      .scale(x0)
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("svg:g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.json("datastudies_pretty.json", function(error, json) {
+    data1 = json["2015"]
+    console.log(data1);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g").selectAll("g")
+      .data(data1)
+    .enter().append("g")
+      .style("fill", function(d, i) { return z(i); })
+      .attr("transform", function(d, i) { return "translate(" + x1(i) + ",0)"; })
+    .selectAll("rect")
+      .data(function(d) { return d; })
+    .enter().append("rect")
+      .attr("width", x1.rangeBand())
+      .attr("height", y)
+      .attr("x", function(d, i) { return x0(i); })
+      .attr("y", function(d) { return height - y(d); });
+      })
+};
